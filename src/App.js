742b2bd797cf3d5957ed3f644da48170d2674b1b
Karly Stark <karlystark@gmail.com>
2023-09-07 14:49:44 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Nav from "./Nav";
+import RoutesList from "./RoutesList";
+import { BrowserRouter } from "react-router-dom";
+import PixlyApi from "./api";
 
 function App() {
+  const [imageData, setImageData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(function fetchImageDataOnMount() {
+    async function fetchImageData() {
+      const data = await PixlyApi.getImages();
+      setImageData(data);
+      setIsLoading(false);
+    }
+    fetchImageData();
+  }, []);
+
+  if (isLoading){return "loading images :)"};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <RoutesList imageData={imageData} />
+      </BrowserRouter>
     </div>
   );
 }
